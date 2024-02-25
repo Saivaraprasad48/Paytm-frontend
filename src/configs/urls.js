f06223b657bc76457eb305cc716c7936c52cfd7e
Sai Vara Prasad Degala <93783719+Saivaraprasad48@users.noreply.github.com>
2024-02-25 17:51:00 +0530

    Updated backend API

--- src/configs/urls.js
+++ src/configs/urls.js
@@ -1,4 +1,5 @@
-const hostname = "http://localhost:5000/api/v1";
+// const hostname = "http://localhost:5000/api/v1";
+const hostname = "https://paytm-backend-ka57.onrender.com/api/v1";
 
 export const endpoints = {
   login: `${hostname}/user/signin`,
